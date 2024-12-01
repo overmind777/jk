edfb97228d982b3e707077d6501f342c26d74e4b
Oleksandr Hembaruk <overmind@Mac-mini-Oleksandr.local>
2024-12-01 12:01:30 +0200

    install redux

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,10 +1,15 @@
+import {Admin, Resource, ListGuesser} from "react-admin";
+import jsonServerProvider from 'ra-data-json-server'
 
 
 const AdminPanel = () => {
-    return (
-        <div>
+    const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-        </div>
+    return (
+        <Admin dataProvider={dataProvider}>
+            <Resource name="posts" list={ListGuesser} />
+            <Resource name="comments" list={ListGuesser} />
+        </Admin>
     );
 };
 
