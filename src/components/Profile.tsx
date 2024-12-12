b6b9df37e1a7e07bd96c37aa88ecc8ae9283ef70
Overmind <overmind@MacBook-Pro-Overmind.local>
2024-12-12 14:39:16 +0200

    add profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,21 +3,23 @@ import styled from 'styled-components';
 const Profile = () => {
     return (
         <Wrapper>
-            <div>
+            <ButtonWrapper>
+                <button>Edit</button>
+            </ButtonWrapper>
+            <ImageWrapper>
                 <img src="./vite.svg" alt="" />
                 <div>
                     <input type="file" accept={ 'image/*' } />
                 </div>
-            </div>
-            <RightWrapper>
+            </ImageWrapper>
+            <InfoWrapper>
                 <h2>www@www.www</h2>
                 <p>Статус: Новачок</p>
                 <div></div>
                 <div></div>
                 <h3>Про мене</h3>
                 <p>Інформація відсутня</p>
-            </RightWrapper>
-            <button>Edit</button>
+            </InfoWrapper>
         </Wrapper>
     );
 };
@@ -27,11 +29,11 @@ export default Profile;
 const Wrapper = styled.div`
     display: grid;
     grid-template-columns: 1fr 3fr;
-    gap: 40px;
+    grid-template-rows: 2fr;
+    gap: 5px;
     align-items: flex-start;
 
     max-width: 1000px;
-    margin: 100px auto 30px;
     padding: 30px;
     background: gray;
     border-radius: 15px;
@@ -39,7 +41,23 @@ const Wrapper = styled.div`
     position: relative;
 `
 
-const RightWrapper = styled.div`
+const ButtonWrapper = styled.div`
+    grid-column-start: 3;
+    grid-row-start: 1;
+    grid-row-end: 1;
+`
+
+const ImageWrapper = styled.div`
+    grid-column-start: 1;
+    grid-row-start: 1;
+    grid-row-end: 2;
+`
+
+const InfoWrapper = styled.div`
+    grid-column-start: 2;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    
     display: flex;
     flex-direction: column;
     gap: 3px;
